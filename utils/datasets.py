831385bb82484f3ff0061619949e1dd60b66c8d4
mizone <401883244@qq.com>
2021-07-12 16:52:10 +0800

    Default Changelist

--- utils/datasets.py
+++ utils/datasets.py
@@ -372,6 +372,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.mosaic_border = [-img_size // 2, -img_size // 2]
         self.stride = stride
         self.path = path
+        self.albumentations = Albumentations() if augment else None
 
         try:
             f = []  # image files
